5669dd1a1567331a9cd3a3b23e94d7f803c910cf
Pov-Yanghai <Yanghai.pov@student.cadt.edu.kh>
2025-03-06 16:05:07 +0700

    Resolved merge conflicts

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print("hello")
 # mr sina edit this place 
 # hello mr sina edit this place why only one line 
+print ("MR sina is the most hansome boy in the world")
+# hello mr sina edit this place why only one line
