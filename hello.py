e3529f8010211e1d0d1d731d27d5ccb3ec08def4
Sinavorn <sinavorn670@gmail.com>
2025-03-06 20:17:21 +0700

    test

--- hello.py
+++ hello.py
@@ -5,3 +5,5 @@ print ("MR sina is the most hansome boy in the world")
 # hello mr sina edit this place why only one line
 
 print("MR Yanghai is the most hansome boy in the world")
+
+# b hai luch sl mnus mneak in DS_G2 because him yy te name her te day ng
