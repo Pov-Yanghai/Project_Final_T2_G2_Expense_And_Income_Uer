649cc2eb945429db8f35c550a76a8969a823d7b8
Sinavorn <sinavorn670@gmail.com>
2025-03-08 18:23:33 +0700

    Final.py

---
+++ FInal.py
@@ -0,0 +1,67 @@
+import csv
+import os
+import sys
+from abc import ABC, abstractmethod
+from datetime import datetime
+import pandas as pd
+
+# Clear screen for Window, Mac and Linux
+def clear_screen():
+    if sys.platform.startswith('win'):
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Transaction class
+class Transaction(ABC):
+    def __init__(self, amount , date , description , currency):
+        self.amount = amount
+        self.date = date
+        self.description = description
+        self.currency = currency
+    @abstractmethod
+    def get_type(self):
+        pass
+    @abstractmethod
+    def __str__(self):
+        pass
+# Income class
+class Income(Transaction):
+    pass
+# Expense class
+class Expense(Transaction):
+    pass
+# Transaction report class
+class Report:
+    pass
+# User class
+class User:
+    pass
+# Admin class
+class Admin:
+    pass
+# Main program
+def main():
+    while True:
+        clear_screen()
+        print("Welcome to our system")
+        print("1. Login")
+        print("2. Register")
+        print("3. Exit")
+        choice = input("Choose option: ")
+        if choice == "1":
+            username = input("Username: ").strip()
+            password = input("Password: ").strip()
+        elif choice == "2":
+            fullname = input("Full name: ")
+            username = input("Username: ").strip()
+            password = input("Password: ").strip()
+        elif choice == "3":
+            print("Goodbye")
+            break
+        else:
+            print("Invalid choice")
+            input("Press any key to continue...")
+
+if __name__ == "__main__":
+    main()
